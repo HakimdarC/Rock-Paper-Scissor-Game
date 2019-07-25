6052b3bc955862c221dc37097709f7bb62b85c84
HakimdarC <45515325+HakimdarC@users.noreply.github.com>
2019-07-26 00:57:17 +0100

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,99 @@
+from random import randint
+list = ["Rock","Paper","Scissors"]
+computer = list[randint(0,2)]
+player = False
+while player == False:
+    player = input("Rock,Paper,Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("you lose!", computer,"cover", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer,"cut", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif Player == "Scissors":
+        if computer == "Rock":
+            print("You Lose..", computer,"smashes",player)
+        else:
+            print("You win!",player,"cut",computer)
+    else:
+        print("Thats not a valid play. check your spelling!")
+list = ["Rock","Paper","Scissors"]
+computer = list[randint(0,2)]
+player = False
+while player == False:
+    player = input("Rock,Paper,Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("you lose!", computer,"cover", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer,"cut", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif Player == "Scissors":
+        if computer == "Rock":
+            print("You Lose..", computer,"smashes",player)
+        else:
+            print("You win!",player,"cut",computer)
+
+            list = ["Rock","Paper","Scissors"]
+computer = list[randint(0,2)]
+player = False
+while player == False:
+    player = input("Rock,Paper,Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("you lose!", computer,"cover", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer,"cut", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif Player == "Scissors":
+        if computer == "Rock":
+            print("You Lose..", computer,"smashes",player)
+        else:
+            print("You win!",player,"cut",computer)
+    else:
+        print("Thats not a valid play. check your spelling!")
+
+        list = ["Rock","Paper","Scissors"]
+computer = list[randint(0,2)]
+player = False
+while player == False:
+    player = input("Rock,Paper,Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("you lose!", computer,"cover", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer,"cut", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif Player == "Scissors":
+        if computer == "Rock":
+            print("You Lose..", computer,"smashes",player)
+        else:
+            print("You win!",player,"cut",computer)
+    else:
+        print("Thats not a valid play. check your spelling!")
+    
+        print("Thats not a valid play. check your spelling!")
